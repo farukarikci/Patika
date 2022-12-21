87390ab40e06b2e62c993a6e3933c7484e79b7a5
farukarikci <88164864+farukarikci@users.noreply.github.com>
2022-12-21 10:57:32 +0300

    Add files via upload

---
+++ donguler_for-loop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace donguler_for_loop{
+
+    class Program {
+
+        static void Main(string [] args){
+            /*
+            
+            for (int i = 1; i <= sayac; i++){
+
+                if(i%2 == 1){
+                    Console.WriteLine(i);
+                }
+            }
+           */
+           int sayac = int.Parse(Console.ReadLine());
+            int ttoplam = 0;
+            int ctoplam = 0;
+
+            for(int i = 1; i <= sayac; i++){
+
+                if(i%2 == 1){
+                     ttoplam = ttoplam +  i;
+
+
+                }
+                if(i%2 == 0){
+                    ctoplam = ctoplam + i;
+                }
+
+                
+            }
+            Console.WriteLine(" Tek Toplam = "+ ttoplam + "Çift Toplam = " + ctoplam );
+
+
+        }
+    }
+}
