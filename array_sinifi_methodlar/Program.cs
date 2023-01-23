d20f55fe8c22c20cbed94ac043142f3417d9f953
farukarikci <88164864+farukarikci@users.noreply.github.com>
2023-01-23 21:26:34 +0300

    Add files via upload

---
+++ array_sinifi_methodlar/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace array_sinifi_methodlar {
+
+    class Program{
+
+        static void Main(string [] args){
+
+            int[] sayiDizisi ={23,34,45,56,67,78,12,23,34,45,98,87,76,65};
+
+         /*  Array.Sort(sayiDizisi);
+
+           foreach(var sayi in sayiDizisi){
+
+            Console.WriteLine(sayi);
+           }
+         */
+
+
+        /*   Array.Clear(sayiDizisi,2,3);
+
+           foreach(var sayi in sayiDizisi){
+
+            Console.WriteLine(sayi);
+           }
+
+         */
+
+
+
+         /* 
+          Array.Reverse(sayiDizisi);  
+         foreach(var sayi in sayiDizisi){
+
+            Console.WriteLine(sayi);
+           } 
+        */
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi,78));
+        }
+    }
+}
